3b97e8d0116769a43dc927ad26e25ae21515b1f2
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-19 18:47:19 -0300

    Update: update header functionalities

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import React from "react";
-export default class Main extends React.Component {
-  render() {
-    return (
-      <header className="header">
+
+import { NavLink } from "react-router-dom";
+export default function Main(props) {
+  return (
+    <header className="header">
+      <div className="header__bar">
         <h2 className="header__title">
           Around
           <sup className="header__span">
             <sup>The U.S.</sup>
           </sup>
         </h2>
-        <div className="header__line"></div>
-      </header>
-    );
-  }
+        <NavLink className="header__link" to={props.pathTo}>
+          {props.registerLoginLink}
+        </NavLink>
+      </div>
+      <div className="header__line"></div>
+    </header>
+  );
 }
