962b063f34f744ebb0c4ce931706fddd9a22fdc1
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-19 18:48:11 -0300

    Update: update Register render

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,48 @@
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import { useContext } from 'react';
+import Header from "./Header";
+import UserDataForm from "./userDataForm";
 
 export default function Register(props) {
-  const currentUser = useContext(CurrentUserContext);
-
   return (
     <>
+      <Header 
+        registerLoginLink="Entrar" 
+        pathTo="/signin" 
+      />
+
+      <UserDataForm
+        title="Inscrever-se"
+        formName="signup"
+        children=""
+        onSubmit=""
+        buttonText="Inscrever-se"
+        linkText="Já é um membro? Faça o login aqui!"
+        pathTo="/signin"
+      >
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          className="auth__input"
+          id="email-input"
+          minLength="2"
+          maxLength="50"
+        />
+        <span className="popup__placeholder name-input-error"></span>
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Senha"
+          required
+          className="auth__input"
+          id="password-input"
+          minLength="2"
+          maxLength="40"
+        />
+
+        <span className="popup__placeholder About-input-error"></span>
+      </UserDataForm>
     </>
   );
 }
