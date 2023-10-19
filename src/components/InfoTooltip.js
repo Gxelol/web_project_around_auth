ca8a30b548dc02c8251feb1a1d33ae29a90a3516
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-19 17:08:50 -0300

    Add: add InfoTooltip component

---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,11 @@
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { useContext } from 'react';
+
+export default function InfoTooltip(props) {
+  const currentUser = useContext(CurrentUserContext);
+
+  return (
+    <>
+    </>
+  );
+}
