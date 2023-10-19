1f25451b63e819d4e1c865ab1cd9cff80748b664
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-19 17:08:58 -0300

    Add: add Login component

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { useContext } from 'react';
+
+export default function Login(props) {
+  const currentUser = useContext(CurrentUserContext);
+
+  return (
+    <>
+      
+    </>
+  );
+}
