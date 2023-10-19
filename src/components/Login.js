e8e611c196aa689ecbca11d6090075f3ca51d120
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-19 18:47:33 -0300

    Update: update login children

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,48 @@
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import { useContext } from 'react';
+import Header from "./Header";
+import UserDataForm from "./userDataForm";
 
 export default function Login(props) {
-  const currentUser = useContext(CurrentUserContext);
-
   return (
     <>
-      
+      <Header 
+        registerLoginLink="Inscrever-se" 
+        pathTo="/signup" 
+      />
+
+      <UserDataForm
+        title="Entrar"
+        formName="signin"
+        children=""
+        onSubmit=""
+        buttonText="Entrar"
+        linkText="Ainda não é membro? Inscreva-se aqui!"
+        pathTo="/signup"
+      >
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          className="auth__input"
+          id="email-input"
+          minLength="2"
+          maxLength="50"
+        />
+        <span className="popup__placeholder name-input-error"></span>
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Senha"
+          required
+          className="auth__input"
+          id="password-input"
+          minLength="2"
+          maxLength="40"
+        />
+
+        <span className="popup__placeholder About-input-error"></span>
+      </UserDataForm>
     </>
   );
 }
