67472232903bf381b150975d1ce3f7db07e88a0a
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-19 17:09:05 -0300

    Add: add Register component

---
+++ src/components/Register.js
@@ -0,0 +1,11 @@
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { useContext } from 'react';
+
+export default function Register(props) {
+  const currentUser = useContext(CurrentUserContext);
+
+  return (
+    <>
+    </>
+  );
+}
