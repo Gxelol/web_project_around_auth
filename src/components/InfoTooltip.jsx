8a43141d305a5e0f880fc7842dcfae15801efdc4
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-23 21:54:03 -0300

    Update: update InfoToolTip component

--- src/components/InfoTooltip.jsx
+++ src/components/InfoTooltip.jsx
@@ -1,11 +1,18 @@
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import { useContext } from 'react';
+import closeIcon from "../images/CloseIconclose.svg";
+import successIcon from "../images/verified.svg";
+import errorIcon from "../images/not-verified.svg";
 
 export default function InfoTooltip(props) {
-  const currentUser = useContext(CurrentUserContext);
-
   return (
     <>
+      <div className={`tooltip ${props.name} ${props.isOpen ? 'tooltip_active' : ''}`}>
+        <button className="tooltip__button" onClick={props.onClose}>
+          <img src={closeIcon} alt="Botão fechar" />
+        </button>
+
+        <img src={props.isSuccess ? successIcon : errorIcon} alt="Ícone de sucesso ou erro" className='tooltip__image'/>
+        <p className='tooltip__text'>{props.isSuccess ? 'Vitória! Você se registou com sucesso.' : props.tooltipMessage}</p>
+      </div>
     </>
   );
 }
